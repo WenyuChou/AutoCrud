6bc6818ed55f27dd27aa78ff4a446268fabe558c
zhouwenyu <zhouwenyu@tom.com>
2020-07-27 18:35:28 +0800

    crud

--- src/main/java/com/zhou/crud/AutoCrudApi.java
+++ src/main/java/com/zhou/crud/AutoCrudApi.java
@@ -12,22 +12,24 @@ import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 /**
  * @author : wenyu.chou@outlook.com
  * @version : 1.0
  * @see #getAuthor() <font color = yellow>在此填入你的信息 crtl + 鼠标左键进入</font><br>
- * @see #createPojo(String, String, String)  model实体类创建源码
- * @see #createDao(String, String, String) mapper创建源码
+ * @see #createPojo(String, String, String,String)  model实体类创建源码
+ * @see #createDao(String, String, String,String) mapper创建源码
  * </br>
  */
 public class AutoCrudApi {
+    private final static Logger log = LoggerFactory.getLogger("AutoCrudApi");
     /**<font color = yellow>请使用下述main方法生成代码。</font>*/
     public static void main(String[] args) {
         //for example:
-        //AutoCrudApi api = new AutoCrudApi();
-        //api.createJavaCode("duojia_4test","s_split_bill");
+        AutoCrudApi api = new AutoCrudApi();
+        api.createJavaCode("duojia_4test","s_split_bill","SplitBill");
     }
     final static String DRIVER = "com.mysql.jdbc.Driver";
 
@@ -66,15 +68,17 @@ public class AutoCrudApi {
      * 数据库操作 (无需修改)
      */
     private static final String SQL = "SELECT * FROM ";
-    private final static Logger log = LoggerFactory.getLogger("AutoCrudApi");
 
     /**----------------------------------------------API---------------------------------------------------------------*/
 
     public void createJavaCode(String database, String tableName) {
-        if (createPojo(this.parentPage, database, tableName)) {
+        this.createJavaCode(database,tableName,null);
+    }
+    public void createJavaCode(String database, String tableName,String createFileName) {
+        if (createPojo(this.parentPage, database, tableName, createFileName)) {
             log.info(pojo + " 创建成功");
         }
-        if (createDao(this.parentPage, database, tableName)) {
+        if (createDao(this.parentPage, database, tableName, createFileName)) {
             log.info(dao + " 创建成功");
         }
     }
@@ -268,7 +272,7 @@ public class AutoCrudApi {
      * @param tableName  数据表名称
      * @return 操作是否成功
      */
-    public boolean createDao(String parentPage, String database, String tableName) {
+    public boolean createDao(String parentPage, String database, String tableName,String createName) {
         String packageName = this.dao;
         File file = new File(System.getProperty("user.dir") + "/src/main/java/" +
                 parentPage.replace(".", "/") + "/" + packageName);
@@ -279,27 +283,27 @@ public class AutoCrudApi {
         }
         //创建dao层
         List<String> columnNames = this.getColumnNames(database, tableName);
-        File dao = new File(file.getAbsoluteFile() + "/" + nameChange(tableName, true) + "Mapper.java");
+        String createClassName = Optional.ofNullable(createName).orElse(nameChange(tableName, true));
+        File dao = new File(file.getAbsoluteFile() + "/" + createClassName + "Mapper.java");
         if (!dao.exists()) {
-            String importJar = "import " + parentPage + "." + this.pojo + "." + nameChange(tableName, true) + ";" +
+            String importJar = "import " + parentPage + "." + this.pojo + "." + createClassName + ";" +
                     "\nimport org.apache.ibatis.annotations.Param;" +
                     "\nimport java.util.List;" +
                     "\nimport java.util.Set;" +
                     "\nimport org.apache.ibatis.annotations.Mapper;\n";
             BufferedWriter bw;
             try {
-                bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile() + "/" + nameChange(
-                        tableName, true) + "Mapper.java"));
+                bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile() + "/" + createClassName + "Mapper.java"));
                 //在创建好的文件中写入"具体代码"
                 bw.write("package " + parentPage + "." + packageName + ";\n" + importJar + "\n" + getAuthor() + "\n" +
-                        "@Mapper\npublic interface " + nameChange(tableName, true) + "Mapper{\n" +
-                        "    int " + this.sqlInsert + "(" + nameChange(tableName, true) + " param);\n" +
+                        "@Mapper\npublic interface " + createClassName + "Mapper{\n" +
+                        "    int " + this.sqlInsert + "(" + createClassName + " param);\n" +
                         "    int " + this.sqlDelete + "(Long " + nameChange(columnNames.get(0), false) + ");\n" +
-                        "    int " + this.sqlUpdate + "(" + nameChange(tableName, true) + " param);\n" +
-                        "    List<" + nameChange(tableName, true) + "> " + this.sqlSelectList + "(" + nameChange(tableName, true) + " param);\n" +
-                        "    List<" + nameChange(tableName, true) + "> " + this.sqlSelectListByIds + "(@Param(\"ids\")Set<Long> ids);\n" +
-                        "    " + nameChange(tableName, true) + " " + this.sqlSelectObject + "(" + nameChange(tableName, true) + " param);\n" +
-                        "    " + nameChange(tableName, true) + " " + this.sqlSelectById + "(Long " + nameChange(columnNames.get(0), false) + ");\n" +
+                        "    int " + this.sqlUpdate + "(" + createClassName + " param);\n" +
+                        "    List<" + createClassName + "> " + this.sqlSelectList + "(" + createClassName + " param);\n" +
+                        "    List<" + createClassName + "> " + this.sqlSelectListByIds + "(@Param(\"ids\")Set<Long> ids);\n" +
+                        "    " + createClassName + " " + this.sqlSelectObject + "(" + createClassName + " param);\n" +
+                        "    " + createClassName + " " + this.sqlSelectById + "(Long " + nameChange(columnNames.get(0), false) + ");\n" +
                         "\n}");
                 //一定要关闭文件
                 bw.close();
@@ -307,7 +311,7 @@ public class AutoCrudApi {
                 e.printStackTrace();
             }
         } else {
-            log.info(nameChange(tableName, true) + "Mapper.java 已经存在(未覆盖，如需重建则先删除此文件)");
+            log.info(createClassName + "Mapper.java 已经存在(未覆盖，如需重建则先删除此文件)");
         }
 
         //mapper.xml文件生成
@@ -316,19 +320,19 @@ public class AutoCrudApi {
             //创建文件夹
             return false;
         }
-        if (new File(mapperPage.getAbsolutePath() + "/" + nameChange(tableName, true) + "Mapper.xml").exists()) {
+        if (new File(mapperPage.getAbsolutePath() + "/" + createClassName + "Mapper.xml").exists()) {
             log.info(mapperPage.getAbsoluteFile() + "/"
-                    + nameChange(tableName, true) + "Mapper.xml 已经存在(未覆盖，如需重建则先删除此文件)");
+                    + createClassName + "Mapper.xml 已经存在(未覆盖，如需重建则先删除此文件)");
             return false;
         }
-        String pojoPath = "\"" + parentPage + "." + this.pojo + "." + nameChange(tableName, true) + "\"";
+        String pojoPath = "\"" + parentPage + "." + this.pojo + "." + createClassName + "\"";
         StringBuilder code = new StringBuilder
                 ("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
                         "<!DOCTYPE mapper PUBLIC \"-//mybatis.org//DTD Mapper 3.0//EN\" \"http://mybatis.org/dtd/mybatis-3-mapper.dtd\">"
                         + "\n<mapper namespace=\"");
-        code.append(parentPage).append(".").append(this.dao).append(".").append(nameChange(tableName, true)).append("Mapper\">\n")
+        code.append(parentPage).append(".").append(this.dao).append(".").append(createClassName).append("Mapper\">\n")
                 .append("<resultMap id=\"BaseResultMap\" type=\"").append(parentPage).append(".").append(this.pojo)
-                .append(".").append(nameChange(tableName, true)).append("\" >");
+                .append(".").append(createClassName).append("\" >");
 
         StringBuilder sql = new StringBuilder("\n<sql id=\"Base_Column_List\" >\n");
 
@@ -399,7 +403,7 @@ public class AutoCrudApi {
                 .append(selectList).append(selectListByIds).append(selectById).append(selectObject).append("\n</mapper>");
         try {
             BufferedWriter bw = new BufferedWriter(new FileWriter(mapperPage.getAbsolutePath() + "/"
-                    + nameChange(tableName, true) + "Mapper.xml"));
+                    + createClassName + "Mapper.xml"));
             bw.write(code.toString());
             bw.close();
         } catch (IOException e) {
@@ -417,7 +421,7 @@ public class AutoCrudApi {
      * @param tableName  数据表名称
      * @return 操作是否成功
      */
-    public boolean createPojo(String parentPage, String database, String tableName) {
+    public boolean createPojo(String parentPage, String database, String tableName,String createName) {
         String packageName = this.pojo;
         File file = new File(System.getProperty("user.dir") + "/src/main/java/" +
                 parentPage.replace(".", "/") + "/" + packageName);
@@ -426,6 +430,7 @@ public class AutoCrudApi {
             //创建文件夹失败
             return false;
         }
+        String createClassName = Optional.ofNullable(createName).orElse(nameChange(tableName, true));
         StringBuilder importJar = new StringBuilder("import java.io.Serializable;\n");
         StringBuilder code = new StringBuilder();
         StringBuilder getterSetter = new StringBuilder();
@@ -441,7 +446,7 @@ public class AutoCrudApi {
                 importJar.append("\nimport java.util.Date;");
             }
             if("BigDecimal".equals(type) && !importJar.toString().contains("java.math.BigDecimal")){
-                importJar.append("\njava.math.BigDecimal;");
+                importJar.append("\nimport java.math.BigDecimal;");
             }
             code.append("\n    ").append("/**").append(desc).append("*/").append("\n    ").append("private ")
                     .append(type).append(" ").append(name).append(";");
@@ -458,17 +463,17 @@ public class AutoCrudApi {
         try {
             //如果文件夹下没有.java就会创建该文件
             File isPojo = new File(file.getAbsoluteFile() + "/"
-                    + nameChange(tableName, true) + ".java");
+                    + createClassName + ".java");
             if (isPojo.exists()) {
                 log.info(file.getAbsoluteFile() + "/"
-                        + nameChange(tableName, true) + ".java 已经存在(未覆盖，如需重建则先删除此文件)");
+                        + createClassName + ".java 已经存在(未覆盖，如需重建则先删除此文件)");
                 return true;
             }
             BufferedWriter bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile() + "/"
-                    + nameChange(tableName, true) + ".java"));
+                    + createClassName + ".java"));
             //在创建好的文件中写入"具体代码"
             bw.write("package " + parentPage + "." + packageName + ";\n" + importJar.toString() + "\n" + getAuthor() + "\npublic class " +
-                    nameChange(tableName, true) + " implements Serializable {\n\n" +
+                    createClassName + " implements Serializable {\n\n" +
                     "    private static final long serialVersionUID = " + new Random().nextLong() + "L;\n" + code.toString() + "\n}");
             //一定要关闭文件
             bw.close();
